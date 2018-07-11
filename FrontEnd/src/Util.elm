f6579902102917c2f52dd66072fabc3e2dcd2392
thibaud <pernet-bissier@intechinfo.fr>
2018-07-11 11:48:20 +0200

    Initialized frontend project

---
+++ FrontEnd/src/Util.elm
@@ -0,0 +1,5 @@
+module Util exposing ((=>), appendErrors, onClickStopPropagation, pair, viewIf)
+
+import Html exposing (Attribute, Html)
+import Html.Events exposing (defaultOptions, onWithOptions)
+import Json.Decode as Decode
